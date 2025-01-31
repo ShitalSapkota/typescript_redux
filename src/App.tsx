39096c0453524e70d906c6b2c11448deb0e87a81
Shital Sapkota <shital.sapkota1@gmail.com>
2025-01-31 10:32:02 +0200

    install redux

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from '@mui/material'
+
+function App() {
+  return (
+    <>
+      <h1>Redux</h1>
+      <Button>Click Me</Button>
+    </>
+  )
+}
+
+export default App
